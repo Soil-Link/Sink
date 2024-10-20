7d645fcd609165756ae246f64d646b316f4e83b3
Jonty Brook <me@jontyb.co.uk>
2024-10-20 14:57:09 +0100

    adjust for subcode

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
-  title: 'Sink',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
+  title: '_subcode Links',
+  description: 'Simple link shortener.',
+  image: 'https://soillink.app/logo.png',
   previewTTL: 24 * 3600, // 24h
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -1,10 +1,5 @@
 <script setup>
 import { BloggerIcon, GitHubIcon, GmailIcon, MastodonIcon, TelegramIcon, XIcon } from 'vue3-simple-icons'
-
-const email = ref(null)
-onMounted(() => {
-  email.value.href = email.value.href.replace('$', '@')
-})
 </script>
 
 <template>
@@ -13,89 +8,16 @@ onMounted(() => {
       <a
         href="/"
         class="text-xl font-black leading-none text-gray-900 select-none dark:text-gray-100 logo"
-        title="Sink"
-      >Sink</a>
+        title="Subcode Links"
+      >_subcode</a>
       <a
         class="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0"
-        href="https://html.zone"
+        href="https://subcode.ventures"
         target="_blank"
-        title="HTML.ZONE"
+        title="_subcode"
       >
-        &copy; {{ new Date().getFullYear() }} Products of HTML.ZONE
+        &copy; {{ new Date().getFullYear() }} Provided by _subcode
       </a>
-      <span
-        class="inline-flex justify-center mt-4 space-x-5 sm:ml-auto sm:mt-0 sm:justify-start"
-      >
-        <a
-          ref="email"
-          href="mailto:sink.cool$miantiao.me"
-          title="Email"
-          class="text-gray-400 hover:text-gray-500"
-        >
-          <span class="sr-only">Email</span>
-          <GmailIcon
-            class="w-6 h-6"
-          />
-        </a>
-        <a
-          href="https://t.me/htmlzone"
-          target="_blank"
-          title="Telegram"
-          class="text-gray-400 hover:text-gray-500"
-        >
-          <span class="sr-only">Telegram</span>
-          <TelegramIcon
-            class="w-6 h-6"
-          />
-        </a>
-        <a
-          href="https://mt.ci"
-          target="_blank"
-          title="Blog"
-          class="text-gray-400 hover:text-gray-500"
-        >
-          <span class="sr-only">Blog</span>
-          <BloggerIcon
-            class="w-6 h-6"
-          />
-        </a>
-
-        <a
-          href="https://x.com/0xKaiBi"
-          target="_blank"
-          title="Twitter"
-          class="text-gray-400 hover:text-gray-500"
-        >
-          <span class="sr-only">Twitter</span>
-          <XIcon
-            class="w-6 h-6"
-          />
-        </a>
-
-        <a
-          href="https://miantiao.me/@chi"
-          target="_blank"
-          title="Mastodon"
-          class="text-gray-400 hover:text-gray-500"
-        >
-          <span class="sr-only">Mastodon</span>
-          <MastodonIcon
-            class="w-6 h-6"
-          />
-        </a>
-
-        <a
-          href="https://github.com/ccbikai"
-          target="_blank"
-          title="GitHub"
-          class="text-gray-400 hover:text-gray-500"
-        >
-          <span class="sr-only">GitHub</span>
-          <GitHubIcon
-            class="w-6 h-6"
-          />
-        </a>
-      </span>
     </div>
   </section>
 </template>
